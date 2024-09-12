ba7d56eb20ae441cfc2bb40afe122e9d8ea38231
yangtao <a1316422632@163.com>
2024-09-12 17:58:50 +0800

    实时轨迹

--- src/pages/main/main.vue
+++ src/pages/main/main.vue
@@ -1,25 +1,43 @@
 <script setup>
 import { initMap, initPolyline } from "../../components/maps/maps";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 
-var path = [
-    [104.087575, 30.65382],
-    [104.088871, 30.653315],
-    [104.089077, 30.65374],
-    [104.089431, 30.654486]
-];
-
+let path = [];
+let tt = ref('');
 let polyline = null;
 // 更新位置函数
-function updatePosition(lat, lng) {
+function updatePosition(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    // console.log([lng, lat])
+    // var point = new window.AMap.LngLat(lng, lat);
+
+    // 更新地图中心
+    // map.setCenter(point);
+    tt.value = tt.value + lat + "," + lng + ";\n";
+    console.log(tt.value)
+    // 更新步行轨迹
     if (polyline == null) {
-        // path.push([lng, lat]);
+        path.push([lng, lat]);
         polyline = initPolyline(path);
     } else {
-        var path1 = polyline.getPath();
-        path1.push([lng, lat]);
-        polyline.setPath(path1);
+        path.push([lng, lat]);
+        polyline.setPath(path);
     }
+}
+// 错误处理
+function handleError(error) {
+    console.error("位置获取失败：", error.message);
+}
+let nn = null | Number;
+
+function start() {
+    // 监控位置变化
+    nn = navigator.geolocation.watchPosition(updatePosition, handleError, {
+        enableHighAccuracy: true
+    });
+} function end() {
+    navigator.geolocation.clearWatch(nn)
 }
 onMounted(() => {
     initMap();
@@ -28,7 +46,12 @@ onMounted(() => {
 <template>
     <view id="container">
         <view class="cc">
-            <up-button text="月落" @click="updatePosition(30.654344,104.09034)"></up-button>
+            <!-- <up-button text="月落" @click="updatePosition(30.654344, 104.09034)"></up-button> -->
+            <up-button text="开始" @click="start()"></up-button>
+            <up-button text="暂停" @click="end()"></up-button>
+        </view>
+        <view class="dd">
+            <text>位置：{{ tt }}</text>
         </view>
     </view>
 </template>
@@ -42,7 +65,13 @@ onMounted(() => {
 .cc {
     position: absolute;
     right: 0;
-    bottom: 0;
-    z-index: 15;
+    bottom: 1;
+    z-index: 20;
+}
+.dd {
+    position: absolute;
+    right: 1;
+    bottom: 1;
+    z-index: 20;
 }
 </style>
